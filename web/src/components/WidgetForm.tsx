1f902ed738971762852c89291d1c831b0eea1617
Railson25 <railsonsantiago123@gmail.com>
2022-05-03 12:53:48 -0300

    Estrutura do WidgetForm

---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,15 @@
+export function WidgetForm() {
+    return(
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+            </header>
+
+            <p> Olá</p>
+
+            <footer className="text-xl text-neutral-400">
+            Feito com ♥ por <a className="underline underline-offset-4" href="https://github.com/Railson25">Railson Santiago</a>
+            </footer>    
+        </div>
+    )
+}
